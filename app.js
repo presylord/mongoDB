3d06419b44cc341ea90f8a5737952419b983382e
presylord <kidlat123@gmail.com>
2021-11-12 16:48:12 +0800

    mid work

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
 });
+const userInfoSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
 
 const User = mongoose.model("users", userSchema);
+const UserInfo = mongoose.model("userInfo", userInfoSchema);
 
 const user = new User({
   name: "Presy Lord",
   email: "kidlat123@gmail.com",
 });
+const userInfo = new UserInfo({
+  name: "Presy Lord",
+  age: 24,
+});
 
 user.save();
+userInfo.save();
